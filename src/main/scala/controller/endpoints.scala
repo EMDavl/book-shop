a1c96261b07dba0e86fa750e10f0322d81956dfc
Emil Davlyatov <emil.davlyatov2014@yandex.ru>
2023-06-06 15:50:42 +0300

    Добавил сваггер эндпоинт

--- src/main/scala/controller/endpoints.scala
+++ src/main/scala/controller/endpoints.scala
@@ -29,18 +29,20 @@ object endpoints {
   val findByIdAuthorEndpoint: Endpoint[Unit, (RequestContext, Id), CommonError, Option[Author], Any] =
     authorsEndpoint.get
       .in(header[RequestContext]("X-Request-Id"))
-      .in(path[Id]("id"))
+      .in(path[Id]("id").description("id автора"))
       .errorOut(jsonBody[CommonError])
       .out(jsonBody[Option[Author]])
+      .description("Метод для получения автора по id")
 
   val findByFirstNameAndLastNameAuthorEndpoint: Endpoint[Unit, (RequestContext, AuthorFirstName, AuthorLastName), CommonError, Option[Author], Any] =
     authorsEndpoint.get
       .in(header[RequestContext]("X-Request-Id"))
       .in("find")
-      .in(query[AuthorFirstName]("firstName"))
-      .in(query[AuthorLastName]("lastName"))
+      .in(query[AuthorFirstName]("firstName").description("Имя автора"))
+      .in(query[AuthorLastName]("lastName").description("Фамилия автора"))
       .errorOut(jsonBody[CommonError])
       .out(jsonBody[Option[Author]])
+      .description("Метод для получения автора по имени и фамилии")
 
   val createAuthorEndpoint: Endpoint[Unit, (RequestContext, AuthorWithoutId), CommonError, Author, Any] =
     authorsEndpoint.post
@@ -48,6 +50,7 @@ object endpoints {
       .in(jsonBody[AuthorWithoutId])
       .errorOut(jsonBody[CommonError])
       .out(jsonBody[Author])
+      .description("Метод для создания автора")
 
   val updateAuthorEndpoint: Endpoint[Unit, (RequestContext, Author), CommonError, Author, Any] =
     authorsEndpoint.put
@@ -55,35 +58,40 @@ object endpoints {
       .in(jsonBody[Author])
       .errorOut(jsonBody[CommonError])
       .out(jsonBody[Author])
+      .description("Метод для обновления автора")
 
   val removeAuthorByIdEndpoint: Endpoint[Unit, (RequestContext, Id), CommonError, Unit, Any] =
     authorsEndpoint.delete
       .in(header[RequestContext]("X-Request-Id"))
       .errorOut(jsonBody[CommonError])
-      .in(path[Id]("id"))
+      .in(path[Id]("id").description("id автора для удаления"))
+      .description("Метод для удаления автора по id")
 
 
   val findPageBookEndpoint: Endpoint[Unit, (RequestContext, Int, Long), CommonError, List[Book], Any] =
     booksEndpoint.get
       .in(header[RequestContext]("X-Request-Id"))
-      .in(query[Int]("limit"))
-      .in(query[Long]("offset"))
+      .in(query[Int]("limit").description("Максимальное количество записей в запросе"))
+      .in(query[Long]("offset").description("Id автора от которого будем отталкиваться при поиске новой страницы"))
       .errorOut(jsonBody[CommonError])
       .out(jsonBody[List[Book]])
+      .description("Метод для пагинированного получения книг")
 
   val findByIdBookEndpoint: Endpoint[Unit, (RequestContext, Id), CommonError, Option[Book], Any] =
     booksEndpoint.get
       .in(header[RequestContext]("X-Request-Id"))
-      .in(path[Id]("id"))
+      .in(path[Id]("id").description("id книги"))
       .errorOut(jsonBody[CommonError])
       .out(jsonBody[Option[Book]])
+      .description("Метод для получения книги по id")
 
   val findAllByAuthorIdEndpoint: Endpoint[Unit, (RequestContext, Id), CommonError, List[Book], Any] =
     booksEndpoint.get
       .in(header[RequestContext]("X-Request-Id"))
-      .in("author" / path[Id](name = "id"))
+      .in("author" / path[Id](name = "id").description("id автора"))
       .errorOut(jsonBody[CommonError])
       .out(jsonBody[List[Book]])
+      .description("Метод для получения всех книг автора по его id")
 
   val updateBookEndpoint: Endpoint[Unit, (RequestContext, Book), CommonError, Book, Any] =
     booksEndpoint.put
@@ -91,12 +99,14 @@ object endpoints {
       .in(jsonBody[Book])
       .errorOut(jsonBody[CommonError])
       .out(jsonBody[Book])
+      .description("Метод для обновления книги")
 
   val removeBookByIdEndpoint: Endpoint[Unit, (RequestContext, Id), CommonError, Unit, Any] =
     booksEndpoint.delete
       .in(header[RequestContext]("X-Request-Id"))
       .errorOut(jsonBody[CommonError])
-      .in(path[Id]("id"))
+      .in(path[Id]("id").description("id книги для удаления"))
+      .description("Метод для удаления книги по id")
 
   val createBookEndpoint: Endpoint[Unit, (RequestContext, BookWithoutId), CommonError, Book, Any] =
     booksEndpoint.post
@@ -104,6 +114,7 @@ object endpoints {
       .in(jsonBody[BookWithoutId])
       .errorOut(jsonBody[CommonError])
       .out(jsonBody[Book])
+      .description("Метод для создания книги")
 
   def swaggerRoute(): HttpRoutes[IO] = {
     val endpoints =
